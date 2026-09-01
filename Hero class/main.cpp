fa6f119a2704a2da065989e889eacdc8182968b8
uf44932-bit <uf44932@mail.harpercollege.edu>
2026-09-01 11:48:01 -0500

    Initial Commit

---
+++ Hero class/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hero class
+//
+//  Created by Anahi Munoz on 9/1/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
